419003e1190e683fcbf0834ba0487722b82418a6
PeterPanY <1425895879@qq.com>
2024-07-08 14:10:31 +0800

    feat: 公共方法库新增getScrollBarWidth获取页面滚动条的宽度方法

--- packages/ryxon-utils/src/doms/scroll.ts
+++ packages/ryxon-utils/src/doms/scroll.ts
@@ -21,6 +21,34 @@ export function scrollLeftTo(
   animate()
 }
 
+// 获取页面滚动条的宽度
+let scrollBarWidth: number
+export const getScrollBarWidth = (namespace: string): number => {
+  if (!isClient) return 0
+  if (scrollBarWidth !== undefined) return scrollBarWidth
+
+  const outer = document.createElement('div')
+  outer.className = `${namespace}-scrollbar__wrap`
+  outer.style.visibility = 'hidden'
+  outer.style.width = '100px'
+  outer.style.position = 'absolute'
+  outer.style.top = '-9999px'
+  document.body.appendChild(outer)
+
+  const widthNoScroll = outer.offsetWidth
+  outer.style.overflow = 'scroll'
+
+  const inner = document.createElement('div')
+  inner.style.width = '100%'
+  outer.appendChild(inner)
+
+  const widthWithScroll = inner.offsetWidth
+  outer.parentNode?.removeChild(outer)
+  scrollBarWidth = widthNoScroll - widthWithScroll
+
+  return scrollBarWidth
+}
+
 /**
  * 在容器元素中滚动，将**选定的**元素定位在容器顶部
  */
