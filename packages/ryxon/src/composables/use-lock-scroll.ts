9f9ab15127cd121e4aa9cfcf457d4269e25f20b6
PeterPanY <1425895879@qq.com>
2024-07-08 14:30:32 +0800

    fix: 优化useLockScroll方法添加样式

--- packages/ryxon/src/composables/use-lock-scroll.ts
+++ packages/ryxon/src/composables/use-lock-scroll.ts
@@ -1,33 +1,12 @@
 import { Ref, watch, onScopeDispose } from 'vue'
 import { isClient } from '@vueuse/core'
-import { getStyle, hasClass, addClass, removeClass } from '@ryxon/utils'
-
-let scrollBarWidth: number
-export const getScrollBarWidth = (namespace: string): number => {
-  if (!isClient) return 0
-  if (scrollBarWidth !== undefined) return scrollBarWidth
-
-  const outer = document.createElement('div')
-  outer.className = `${namespace}-scrollbar__wrap`
-  outer.style.visibility = 'hidden'
-  outer.style.width = '100px'
-  outer.style.position = 'absolute'
-  outer.style.top = '-9999px'
-  document.body.appendChild(outer)
-
-  const widthNoScroll = outer.offsetWidth
-  outer.style.overflow = 'scroll'
-
-  const inner = document.createElement('div')
-  inner.style.width = '100%'
-  outer.appendChild(inner)
-
-  const widthWithScroll = inner.offsetWidth
-  outer.parentNode?.removeChild(outer)
-  scrollBarWidth = widthNoScroll - widthWithScroll
-
-  return scrollBarWidth
-}
+import {
+  getStyle,
+  hasClass,
+  addClass,
+  removeClass,
+  getScrollBarWidth
+} from '@ryxon/utils'
 
 export function useLockScroll(
   // rootRef: HTMLElement | Ref<HTMLElement | undefined>,
